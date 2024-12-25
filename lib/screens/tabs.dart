9e3b1d9285f3f57bdc79f975bb5fc342f8fe1fc4
Ali Rhayem <alirhayem.1212@gmail.com>
2024-12-25 19:52:54 +0200

    feat: add bottom navigation for switching between Categories and Favorites screens

--- lib/screens/tabs.dart
+++ lib/screens/tabs.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:meals/screens/categories.dart';
+import 'package:meals/screens/meals.dart';
 
 class TabsScreen extends StatefulWidget {
   const TabsScreen({super.key});
@@ -10,11 +12,33 @@ class TabsScreen extends StatefulWidget {
 }
 
 class _TabsScreen extends State<TabsScreen> {
+  int _selectedPageIndex = 0;
+
+  void _selectPage(int index){
+    setState(() {
+      _selectedPageIndex = index;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
+    Widget activePage = CategoriesScreen();
+
+    if(_selectedPageIndex == 1){
+      activePage = MealsScreen(title: 'Favorites', meals: []);
+    }
+
     return Scaffold(
       appBar: AppBar(
         title: const Text('dynamic'),
+      ),
+      body: activePage,
+      bottomNavigationBar: BottomNavigationBar(
+        onTap: (index){_selectPage(index);},
+        items: const [
+          BottomNavigationBarItem(icon: Icon(Icons.set_meal), label:'Categories'),
+          BottomNavigationBarItem(icon: Icon(Icons.star), label:'Favorites'),
+        ],
       ),
     );
   }
